be1b8994ee1f069bf405f714790a73d7d6626546
Melonchanism <121840186+Melonchanism@users.noreply.github.com>
2026-03-29 18:17:50 -0600

    Initial Commit

---
+++ Mission Command/Mission_CommandApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mission_CommandApp.swift
+//  Mission Command
+//
+//  Created by josh on 3/29/26.
+//
+
+import SwiftUI
+
+@main
+struct Mission_CommandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
